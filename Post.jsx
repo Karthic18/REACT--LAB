19d187b7fbef0ccb51f1a8c8e3dcd070f05ce89a
KARTHIC V <karthikk28661@gmail.com>
2026-02-17 20:27:34 -0800

    PROGRAM 10
    Signed-off-by: KARTHIC V  <karthikk28661@gmail.com>

---
+++ Post.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Post() {
+  return (
+    <div>
+      <p> Virat Kohli The king,Legend,Chase Master,Roll Model,Insperation,Back Bone of Indian cricket team in last 2 era</p>
+    </div>
+  )
+}
+
+export default Post
